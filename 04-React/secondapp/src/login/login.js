0236a2e24d42564d567ef3547db1e1e7a7880cf4
Sainadha Krishna <sathyadevasainadh@gmail.com>
2021-06-08 11:21:40 +0530

    Basic Onclick Event handling using reference variables for inputs

--- 04-React/secondapp/src/login/login.js
+++ 04-React/secondapp/src/login/login.js
@@ -1,16 +1,24 @@
+import React from "react";
+
 export function SignIn(){
+    let un = React.createRef();
+    let pwd = React.createRef();
+    function login()
+    {
+        alert("Username: " + un.current.value + "\nPassword: " + pwd.current.value)
+    }
     return(
         <div>
             <div>
                 <label>Username:</label>
-                <input type="text" name="username"></input>
+                <input type="text" name="username" ref={un}></input>
             </div>
             <div>
                 <label>Password:</label>
-                <input type="password" name="password"></input>
+                <input type="password" name="password" ref={pwd}></input>
             </div>
             <div>
-                <button>Sign In</button>
+                <button onClick={login}>Sign In</button>
             </div>
         </div>
     );
