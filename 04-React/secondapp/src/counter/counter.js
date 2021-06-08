2676518acf153493385c3971fcb7133a7737ced4
Sainadha Krishna <sathyadevasainadh@gmail.com>
2021-06-08 12:58:12 +0530

    Hands on for state management using function and class components..

--- 04-React/secondapp/src/counter/counter.js
+++ 04-React/secondapp/src/counter/counter.js
@@ -1,19 +1,22 @@
 import React from "react";
 
-export function Counter(){
-    let [counter,setCounter] = React.useState(0);
-
-    function increment(){
-        setCounter(++counter);
+export class Counter extends React.Component {
+    //let [counter,setCounter] = React.useState(0);
+    state = {counter : 0};
+    increment = () => {
+        this.setState({counter : ++this.state.counter});
     }
-    function decrement(){
-        setCounter(--counter);
+    decrement = ()=>{
+        this.setState({counter : --this.state.counter});
     }
+    render(){
     return(
         <div>
-            <h3>Counter: {counter}</h3>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
+            <h3>Counter: {this.state.counter}</h3>
+            <button onClick={this.increment}>Increment</button>
+            <button onClick={this.decrement}>Decrement</button>
+            <h2>Done using a class component </h2>
         </div>
-    );
+        );
+    }
 }
